377ba8d3069f46287a28046b4484836e9836aa08
Thomas <hakerh403@gmail.com>
2020-10-29 06:53:44 +0100

    commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+A tool for checking, verifying, archiving and studying mathematical proofs.
---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const O = require('omikron');

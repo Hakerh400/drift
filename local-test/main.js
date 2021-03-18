6a62cc1e11e9fa3cfc50d9fe7833f76f37d2c2c2
Thomas <hakerh403@gmail.com>
2021-03-18 16:37:30 +0100

    commit

---
+++ local-test/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const O = require('omikron');
+const drift = require('..');
+
+const {Database} = drift;
+
+const main = () => {
+  const {db} = drift;
+
+  const t = O.now;
+  const info = O.rec(drift.reduceIdent, 'main');
+  const dt = O.now - t;
+
+  const str = O.rec(drift.info2str, info);
+
+  log(str);
+  
+  log();
+  log(db.size);
+  log();
+  log((dt / 1e3).toFixed(3));
+  // db.save();
+  // const db = Database.load();
+  // db.insert(info);
+  // db.save();
+
+  // const arr = db.getInfo(drift.ident2sym('th.prop.impl.refl')).reducedTo.reducedFrom;
+
+  // log();
+  // log(arr.map(a => O.rec(drift.info2str, a)).reverse().join('\n'))
+};
+
+main();
